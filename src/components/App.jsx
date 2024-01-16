9c8452c05801680f1470fdd1870138faca050a0d
OlexandrJ <134488937+OlexandrJ@users.noreply.github.com>
2024-01-16 15:05:14 +0200

    all config

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,34 @@
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { refreshUserThunk } from '../redux/dataUser/userThunk';
+import Navigation from '../components/Navigation/Navigation';
+import { PublicRoute } from '../components/Public/PublicRoute';
+import SignUp from '../pages/SignUp';
+import Login from '../pages/Login';
+import { PrivateRoute } from './Private/PrivateRoute';
+import Contacts from '../pages/Contacts';
+
 export const App = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(refreshUserThunk());
+  }, [dispatch]);
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<PublicRoute />}>
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/login" element={<Login />} />
+        </Route>
+        <Route path="/" element={<PrivateRoute />}>
+          <Route path="/contacts" element={<Contacts />} />
+        </Route>
+      </Routes>
+    </>
   );
 };
+
+export default App;
